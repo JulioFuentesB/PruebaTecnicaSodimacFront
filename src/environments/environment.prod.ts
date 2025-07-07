11d7d9fd73be5dc3577a7627cf352fe8f907ecad
Julio Fuentes B <76795396+JulioFuentesB@users.noreply.github.com>
2025-07-06 22:39:00 -0500

    ✅ feat →Ajuste enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production       : true,
   environmentName  : 'Prod',
   version          : '1.0.0',
-   apiUrl           : 'https://localhost:7209/api/',
-  //piUrl           : 'https://pruebatecnicasodimacback-f8g4dsacg0hehdh5.mexicocentral-01.azurewebsites.net/api/',
+   //apiUrl           : 'https://localhost:7209/api/',
+  piUrl           : 'https://pruebatecnicasodimacback-f8g4dsacg0hehdh5.mexicocentral-01.azurewebsites.net/api/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,6 @@ export const environment = {
   production       : false,
   environmentName  : 'Dev',
   version          : '1.0.0',
-   apiUrl           : 'https://localhost:7209/api/',
-  //piUrl           : 'https://pruebatecnicasodimacback-f8g4dsacg0hehdh5.mexicocentral-01.azurewebsites.net/api/',
-
+   //apiUrl           : 'https://localhost:7209/api/',
+  piUrl           : 'https://pruebatecnicasodimacback-f8g4dsacg0hehdh5.mexicocentral-01.azurewebsites.net/api/',
 };
