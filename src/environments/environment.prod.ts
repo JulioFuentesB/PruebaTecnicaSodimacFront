a092ea1eb2496e8f6ce75c1ec861c99a42f51122
Julio Fuentes B <76795396+JulioFuentesB@users.noreply.github.com>
2025-07-06 22:47:42 -0500

    🐛 fix → Corregir corregir nombre api en enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   environmentName  : 'Prod',
   version          : '1.0.0',
    //apiUrl           : 'https://localhost:7209/api/',
-  piUrl           : 'https://pruebatecnicasodimacback-f8g4dsacg0hehdh5.mexicocentral-01.azurewebsites.net/api/',
+  apiUrl           : 'https://pruebatecnicasodimacback-f8g4dsacg0hehdh5.mexicocentral-01.azurewebsites.net/api/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   environmentName  : 'Dev',
   version          : '1.0.0',
    //apiUrl           : 'https://localhost:7209/api/',
-  piUrl           : 'https://pruebatecnicasodimacback-f8g4dsacg0hehdh5.mexicocentral-01.azurewebsites.net/api/',
+  apiUrl           : 'https://pruebatecnicasodimacback-f8g4dsacg0hehdh5.mexicocentral-01.azurewebsites.net/api/',
 };
